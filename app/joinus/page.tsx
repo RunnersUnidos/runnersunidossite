9a09326687939d642e58a293a30bfde884c59553
Oscar Leal <oscarleal234@gmail.com>
2024-11-22 15:08:24 -0600

    edited form ui

--- app/joinus/page.tsx
+++ app/joinus/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import UserForm from '../components/form';
-
+// import Logo from '@/public/LOGOLONG.png';
+// import Image from 'next/image';
 const JoinUs = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-around">
+    <div className="h-screen flex flex-col items-center justify-evenly">
       <h1 className="mb-5 text-5xl font-bold text-black">Run With Us!</h1>
-      <ul className="text-2xl text-center font-sans">
-        <li>Trinity Overlook Trail</li>
-        <li> 📌 110 W Commerce Street</li>
-      </ul>
+      {/* <Image
+        src={Logo.src}
+        alt="Logo"
+        width={200}
+        height={100}
+        className="rounded-lg object-cover"
+        quality={100}
+      /> */}
       <UserForm />
     </div>
   );
