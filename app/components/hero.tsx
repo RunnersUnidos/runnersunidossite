38259249555fb073f53832587e7f64c879ed44f9
Oscar Leal <oscarleal@Oscars-MacBook-Pro.local>
2024-11-24 16:57:22 -0600

    fixed layout for desktop

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,10 +2,9 @@
 import React from 'react';
 import Image from 'next/image';
 import RunnersImage from '@/public/runners.jpg';
+import LogoImage from '@/public/WHITELOGOPNG.png';
 import { useRouter } from 'next/navigation';
-interface Image {
-  src: string;
-}
+
 const HomeHero = () => {
   const router = useRouter();
   return (
@@ -21,23 +20,31 @@ const HomeHero = () => {
 
       <div className="absolute inset-0 bg-black/60 z-10"></div>
 
-      <div className="relative z-20 flex items-center justify-center min-h-screen">
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold text-white">
-              RunnersUnidos
-            </h1>
-            <p className="text-xl text-white">
-              A community of runners who come together to share miles, stories,
-              and traditions.
-            </p>
-            <button
-              className="btn bg-orange-300 text-black mt-4"
-              onClick={() => router.push('/joinus')}
-            >
-              Join Us
-            </button>
-          </div>
+      <div className="flex flex-col justify-start items-center z-20 relative gap-4"> {/* Changed to justify-center */}
+        <Image
+          src={LogoImage.src}
+          alt="Logo Image"
+          width={400}
+          height={200}
+          className=""
+        />
+        <p className="text-xl text-white text-center">
+          A community of runners who come together to share miles, stories,
+          and traditions.
+        </p>
+        <div className="flex flex-row gap-4">
+          <button
+            className="btn font-sans bg-transparent hover:bg-pink-500 hover:text-black border-pink-500 text-white mt-4 text-center"
+            onClick={() => router.push('/schedule')}
+          >
+            Schedule
+          </button>
+          <button
+            className="btn font-sans bg-transparent hover:bg-pink-500 hover:text-black border-pink-500 text-white mt-4 text-center"
+            onClick={() => router.push('/joinus')}
+          >
+            Join Us
+          </button>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
+'use client'
 import Image from 'next/image';
 import HomeHero from './components/hero';
 import EventSection from './components/eventsection';
 import ScheduleSection from './components/schedulesection';
 import LaFamiliaSection from './components/lafamiliasection';
 import HeroVideo from './components/heroVideo';
+import { useEffect, useState } from 'react';
 export default function Home() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); 
+    };
+
+    handleResize(); 
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize); 
+    };
+  }, []);
+
   return (
     <main>
-      <HeroVideo />
-      {/* <HomeHero /> */}
+        {isMobile ? <HeroVideo /> : <HomeHero />}
       <LaFamiliaSection />
       <ScheduleSection />
       <EventSection />
