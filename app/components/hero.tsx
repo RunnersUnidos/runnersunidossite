a6518da6ca443cf5b09b609ff1e1ae2bc9082019
Oscar Leal <oscarleal234@gmail.com>
2024-11-27 13:11:00 -0600

    adjusted desktop hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -34,7 +34,7 @@ const HomeHero = () => {
           A community of runners who come together to share miles, stories, and
           traditions.
         </p>
-        <div className="flex flex-row gap-4">
+        {/* <div className="flex flex-row gap-4">
           <button
             className="btn font-sans bg-transparent hover:bg-pink-500 hover:text-black border-pink-500 text-white mt-4 text-center"
             onClick={() => router.push('/schedule')}
@@ -47,7 +47,7 @@ const HomeHero = () => {
           >
             JoinUs
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
