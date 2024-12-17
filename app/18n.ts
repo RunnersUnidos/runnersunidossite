ddecfc96ee52091051106a9ea7eec4d1100466e8
Oscar Leal <oscarleal234@gmail.com>
2024-12-16 22:00:08 -0600

    working on translation button

--- app/18n.ts
+++ app/18n.ts
@@ -1,3 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { locales } from './config';
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  if (!locales.includes(requestLocale as any)) notFound();
+
+  return {
+    messages: (await import(`./messages/${requestLocale}.json`)).default,
+  };
+});
