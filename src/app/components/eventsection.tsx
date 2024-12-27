2abc1444701a574a6c841c92e0c13bfa32e2f52b
Oscar Leal <oscarleal234@gmail.com>
2024-12-26 20:47:53 -0600

    fixed button locale

--- src/app/components/eventsection.tsx
+++ src/app/components/eventsection.tsx
@@ -3,11 +3,13 @@ import Image from 'next/image';
 import SkullImage from '@/public/DiaDeLosMuertos.jpg';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import { useLocale } from 'next-intl';
 interface Image {
   src: string;
 }
 const EventSection = () => {
   const t = useTranslations('DiaDeLosMuertos');
+  const locale = useLocale();
   return (
     <section className="container mx-auto px-4 py-16">
       <div className="grid lg:grid-cols-2 gap-8 items-center">
@@ -25,14 +27,14 @@ const EventSection = () => {
           <p className="font-sans mb-8 text-xl">{t('paragraph')}</p>
           <div className="flex flex-col sm:flex-row gap-4">
             <Link
-              href={'/events/diadelosmuertos'}
+              href={`${locale}/diadelosmuertos`}
               className="btn font-sans btn-outline btn-secondary"
             >
               {t('button1')}
             </Link>
             <Link
               className="font-sans btn bg-pink-500 hover:bg-transparent text-black"
-              href={'/events'}
+              href={`${locale}/events`}
             >
               {t('button2')}
             </Link>
