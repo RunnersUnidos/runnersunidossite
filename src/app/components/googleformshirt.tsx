bc8f456f88acd1d4ab4f79723e5626de06b06441
Oscar Leal <oscarleal234@gmail.com>
2025-03-26 15:35:15 -0500

    size of form adjusted

--- src/app/components/googleformshirt.tsx
+++ src/app/components/googleformshirt.tsx
@@ -5,8 +5,9 @@ const GoogleFormShirt = () => {
     <div className="flex justify-center">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSf0JQJf9ZQdOtxtzh8O9hY8bEwgmx6Zb2RmQKkc2y8stq1P0g/viewform?embedded=true"
-        width="640"
-        height="382"
+        width="100%"
+        height="100%"
+        style={{ minHeight: '382px' }}
       >
         Loading…
       </iframe>
