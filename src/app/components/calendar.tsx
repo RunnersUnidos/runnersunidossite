4d7565bd5fccf181ec384f42fabcea058aa2e842
Oscar Leal <oscarleal234@gmail.com>
2025-04-04 15:43:17 -0500

    added fix

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -1,12 +1,13 @@
+import { useTranslations } from 'next-intl';
 import React from 'react';
-
+import { useLocale } from 'next-intl';
 const Calendar = () => {
+  const t = useTranslations('Calendar');
+  const locale = useLocale();
   return (
     <div className="flex flex-col items-center gap-4 py-8 px-4">
-      <h2 className="text-3xl font-bold text-center">Upcoming Events</h2>
-      <p className="text-gray-600 text-center mb-4">
-        Stay updated with all Runners Unidos events and activities
-      </p>
+      <h2 className="text-5xl font-bold text-center">{t('title')}</h2>
+      <p className="text-gray-800 text-center mb-4">{t('paragraph')}</p>
       <div className="w-full max-w-6xl">
         <iframe
           className="w-full h-[600px] md:h-[800px]"
